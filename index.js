50da9e31f21b583a80f6706d274a82b75418988d
Md Nasemul Islam <83567564+nasemul1@users.noreply.github.com>
2024-09-16 22:04:35 +0600

    modified files

--- index.js
+++ index.js
@@ -12,22 +12,10 @@ const MONGODB_URI = process.env.MONGODB_URI;
 // Middleware
 app.use(express.json());
 
-// app.use(cors({
-//     origin: '*',
-//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-// }));
-
 app.use(cors({
     origin: 'https://jobs-page-pied.vercel.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true,
-    optionsSuccessStatus: 200
 }));
-
-// Handle preflight requests
-app.options('*', cors());
-
-
 
 mongoose.connect(MONGODB_URI, {
     useNewUrlParser: true,
